d7d2d6a321c776f54d8692fd188dd534978658d0
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-10-19 18:57:39 -0500

    Update show_db_contents.php

--- lab9/show_db_contents.php
+++ lab9/show_db_contents.php
@@ -1,12 +1,3 @@
 <?php
-$conn = mysqli_connect("127.0.0.2","root","","test");
-if(!$conn)
-{
-    die("Error". mysqli_connect_error());
-}
-else
-{
-    echo "connection established".'<br>';
-}
-
+echo 'hello world'
 ?>
