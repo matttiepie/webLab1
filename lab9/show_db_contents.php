c33567ed44e56c68c649d6e44174f87cbf935104
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-10-19 22:13:06 -0500

    Update show_db_contents.php

--- lab9/show_db_contents.php
+++ lab9/show_db_contents.php
@@ -1,13 +1,23 @@
 <?php
-echo 'hello world'
-$conn = mysqli_connect("partygoer.mysql.database.azure.com","matthewmartinez","1qaz2wsx!QAZ@WSX","herewego");
-if(!$conn)
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$db_host="partygoer.mysql.database.azure.com";        //Change this
+$db_user="matthewmartinez";        //Change this
+$db_pass="1qaz2wsx!QAZ@WSX";        //Change this
+$db_name="herewego";     //Do not change
+
+$connection = mysqli_connect("partygoer.mysql.database.azure.com","matthewmartinez","1qaz2wsx!QAZ@WSX","herewego");
+if (mysqli_connect_errno())
 {
-    die("Error". mysqli_connect_error());
-}
-else
-{
-    echo "connection established".'<br>';
+    echo 'Connection to database failed:'.mysqli_connect_error();
+    exit();
 }
 
+echo "database connection success<br>";
+echo "<strong>now showing results from a database query...</strong>";
+
 ?>
+
