c7c830bfcf3e12bbdb332f9314162c45f5602942
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-10-24 11:17:39 -0500

    Create save_data.php

---
+++ lab10/save_data.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the first and last names from the form
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+
+    // Create a data string to write to the file
+    $data = $firstName . ' ' . $lastName . "\n";
+
+    // Specify the path to the non-public folder where the data will be stored
+    $filePath = 'data/data.txt';
+
+    // Open the file for appending
+    $file = fopen($filePath, 'a');
+
+    if ($file) {
+        // Write the data to the file
+        fwrite($file, $data);
+
+        // Close the file
+        fclose($file);
+
+        echo 'Data saved successfully.';
+    } else {
+        echo 'Failed to open the file for writing.';
+    }
+} else {
+    echo 'Invalid request.';
+}
+?>
