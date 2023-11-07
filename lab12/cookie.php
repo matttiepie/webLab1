4178c7e4600a7c3ed89aef5b23c4900b429d342d
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-11-06 18:20:33 -0600

    Update cookie.php

--- lab12/cookie.php
+++ lab12/cookie.php
@@ -5,7 +5,7 @@ if (isset($_COOKIE['cs4413'])) {
     echo "The 'cs4413' cookie is already set with the value: " . $_COOKIE['cs4413'];
 } else {
     // Cookie is not set, so let's set it
-    $cookieValue = "example_value";
+    $cookieValue = "This is cookie set";
     $expirationTime = time() + 3600; // 1 hour from now
 
     setcookie('cs4413', $cookieValue, $expirationTime, '/');
