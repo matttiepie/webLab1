d0f27a9bd44a1732d08bbaf009c343d9f6779f90
DanielSigala <139706481+DanielSigala@users.noreply.github.com>
2023-11-26 20:05:50 -0600

    Update checkout.php

--- Botanical-Buddies/checkout.php
+++ Botanical-Buddies/checkout.php
@@ -20,8 +20,40 @@ if (isset($_SESSION['user_id'])) {
     echo $_COOKIE["price"]; 
     echo "   ";
     echo $_COOKIE["items"]; 
+    echo'<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Botanical Buddies</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+            <div>
+            <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700" rel="stylesheet" type="text/css">
+	<style>
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+	</style>
+	<link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+</head>
+<body>
+	<header class="site-header" id="header">
+		<h1 class="site-header__title" data-lead-id="site-header-title">THANK YOU!</h1>
+	</header>
 
-    $sql = "UPDATE `plant_data` SET plant_quantity_available = (plant_quantity_available - plant_quantity)";
+	<div class="main-content">
+		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+		<p class="main-content__body" data-lead-id="main-content-body">
+            Thank you for your order! We will get everything packaged and sent to you in no time!</p>
+            <a href="index.html">Click here to continue Shopping</a>
+	</div>
+            </div>
+            <div id="container" class="carousel"></div>
+        </main>
+    </body>
+</html>';
+
+    $sql = "UPDATE plant_data SET plant_quantity_available = (plant_quantity_available - plant_quantity)";
     $statement = $pdo->prepare($sql);
     $statement->execute();  
 
