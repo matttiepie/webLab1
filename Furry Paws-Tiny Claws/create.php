a3a1da050bbe0e72f1a6fd7613a750f281deb749
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-11-27 12:54:03 -0600

    Update create.php

--- Furry Paws-Tiny Claws/create.php
+++ Furry Paws-Tiny Claws/create.php
@@ -1,25 +1,29 @@
 <?php
+session_start();
+$USERNAME = $_POST['email'];
+$PASSWORD = $_POST['password'];
 $db_host = "partygoer.mysql.database.azure.com"; // Change this
 $db_user = "matthewmartinez"; // Change this
 $db_pass = "1qaz2wsx!QAZ@WSX"; // Change this
 $db_name = "herewego"; // Do not change
 
 $connection = mysqli_connect("$db_host", "$db_user", "$db_pass", "$db_name");
-
 if (!$connection) {
     die("Error: " . mysqli_connect_error());
 }
-$query = "INSERT INTO ENTRY (username, password) VALUES ('$USERNAME', '$PASSWORD')";
-$search="SELECT * FROM ENTRY WHERE username='$USERNAME'";
-$result = mysqli_query($connection, $search);
+
+$query = " SELECT * FROM login where USERNAME = '$USERNAME'";
+$result = mysqli_query($connection, $query);
+
+if (!$result) {
+    die("Error in query: " . mysqli_error($connection));
+}
 if(mysqli_num_rows($result) > 0){
-echo 'Login Already Exist';
-header('refresh: 5; url=login.html');
+    header('refresh: 5, url=createAccount.html');
 }
 else{
-$stmt=mysqli_query($connection, $query);
-echo 'Account Created';
-header('refresh: 5; url=login.html');
+    $insert = "INSERT INTO login values('$USERNAME',$PASSWORD)";
+    mysqli_query($connection, $insert);
+    header('Location: login.html');
 }
-
 ?>
